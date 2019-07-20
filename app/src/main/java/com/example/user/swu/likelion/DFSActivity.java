cbe8d5ca3de4044017bb2cb45976273ca320389c
mintae9 <mintae9@likelion.org>
2019-07-20 20:13:06 +0900

    190720 태영 혼잡도DB연동(아직 테스트만 완료)

--- app/src/main/java/com/example/user/swu/likelion/DFSActivity.java
+++ app/src/main/java/com/example/user/swu/likelion/DFSActivity.java
@@ -1,12 +1,23 @@
 package com.example.user.swu.likelion;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -101,6 +112,20 @@ public class DFSActivity extends AppCompatActivity {
     /////////////////////////////////////////////////////
     String[] index2Time = {"time0530","time0600","time0630","time0700","time0730","time0800","time0830","time0900","time0930","time1000","time1030","time1100","time1130","time1200","time1230","time1300","time1330","time1400","time1430","time1500","time1530","time1600","time1630","time1700","time1730","time1800","time1830","time1900","time1930","time2000","time2030","time2100","time2130","time2200","time2230","time2300","time2330","time2400","time2430"};
 
+    ///////////////////////////////혼잡도 DB
+    // 혼잡도 DB 불러오기
+    final String DATABASE_NAME = "congestion.db";
+    final String PACKAGE_DIRECTORY = "/data/data/com.example.user.swu.likelion/";
+    Context mContext = this;
+    myDBHelper myHelper;
+    SQLiteDatabase congestionDB;
+
+    // 배열선언
+    //String[] arrayStationNo;
+
+    // 배열 사이즈
+    int rowSize;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -113,9 +138,9 @@ public class DFSActivity extends AppCompatActivity {
         int hourInt = Integer.parseInt(hour);
         int minuteInt = Integer.parseInt(minute);
 
-        returnTime(hourInt, minuteInt);
+        String realTime = returnTime(hourInt, minuteInt);
 
-        String fullTime = hour+minute;
+        //String fullTime = hour+minute;
 
         DFSAlgorithm d = new DFSAlgorithm();
         ////////////////////////1호선////////////////////////////
@@ -575,6 +600,31 @@ public class DFSActivity extends AppCompatActivity {
         for(int i=0; i<j; i++){
             queue.poll().print();
         }
+
+        //////////////////////////////DB검색
+        boolean bResult = isCheckDatabase();    // DB가 있는지?
+        Log.d("Working", "DataBase Check=" + bResult);
+        if (!bResult) {  // DB가 없으면 복사
+            copyDataBase();
+        }
+
+        myHelper = new DFSActivity.myDBHelper(this);
+        congestionDB = myHelper.getReadableDatabase();
+        Cursor cursor;
+        cursor = congestionDB.rawQuery("SELECT "+realTime+" FROM satUp where stationName=" + "'시청' AND line=1" , null);
+        rowSize = cursor.getCount();
+        //Log.d(TAG,"rowSize => "+rowSize);
+        int i = 0;
+        String result="";
+        while(cursor.moveToNext()){
+            // 배열 삽입
+            result = cursor.getString(0);
+            i++;
+        }
+
+        Log.d(TAG,"rowSize => "+rowSize+" & result => "+result);
+        cursor.close();
+        congestionDB.close();
     }
 
     public String returnTime(int hourInt, int minuteInt){
@@ -814,6 +864,71 @@ public class DFSActivity extends AppCompatActivity {
             limit -= 1;
         }
     }
+
+    // DB관련 코드
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context){
+            super(context, "congestion.db", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+    }
+
+    public boolean isCheckDatabase() {
+        String filePath = PACKAGE_DIRECTORY + "/databases/" + DATABASE_NAME;
+        File file = new File(filePath);
+        if (file.exists()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void copyDataBase(){
+        Log.d("Working", "copyDatabase");
+        AssetManager manager = mContext.getAssets();
+        String folderPath = PACKAGE_DIRECTORY + "/databases";
+        String filePath = PACKAGE_DIRECTORY + "/databases/" + DATABASE_NAME;
+        File folder = new File(folderPath);
+        File file = new File(filePath);
+        FileOutputStream fileOut = null;
+        BufferedOutputStream bufferOut = null;
+        try {
+            InputStream inputStr = manager.open(DATABASE_NAME);
+            BufferedInputStream bufferStr = new BufferedInputStream(inputStr);
+            if (folder.exists()) {
+
+            }else{
+                folder.mkdirs();
+            }
+            if (file.exists()) {
+                file.delete();
+                file.createNewFile();
+            }
+
+            fileOut = new FileOutputStream(file);
+            bufferOut = new BufferedOutputStream(fileOut);
+            int read = -1;
+            byte[] buffer = new byte[1024];
+            while ((read = bufferStr.read(buffer, 0, 1024)) != -1) {
+                bufferOut.write(buffer, 0, read);
+            }
+
+            bufferOut.flush();
+            bufferOut.close();
+            fileOut.close();
+            bufferStr.close();
+            inputStr.close();
+        } catch (IOException e) {
+            Log.e("Error : ", e.getMessage());
+        }
+    }
+
 }
 class structInfo{
     Stack<String> stations = new Stack();
