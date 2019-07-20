5eb851a9c8aec8aabb0b0afb8a78b8e0e9886134
yuvm02 <yuvm0212@gmail.com>
2019-07-20 10:40:58 +0900

    0720 예진

--- app/src/main/java/com/example/user/swu/likelion/FiveRoadActivity.java
+++ app/src/main/java/com/example/user/swu/likelion/FiveRoadActivity.java
@@ -1,13 +1,22 @@
 package com.example.user.swu.likelion;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FiveRoadActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager pager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_five_road);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        pager = findViewById(R.id.pager);
     }
 }
